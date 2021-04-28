39c1ebcd045d7cfddba8dabc7d38cbec22bc230a
BladeHiker <duanjihang@live.com>
2021-04-28 17:53:36 +0800

    add slot

--- untitled/Elevator.cpp
+++ untitled/Elevator.cpp
@@ -4,134 +4,112 @@
 
 #include "Elevator.h"
 Elevator::Elevator() {
-    this->maxW = 10800;
-    this->place = 0;
-    this->direction = false;
+  this->maxW = 10800;
+  this->place = 0;
+  this->direction = false;
 }
 
-
 void Elevator::press(int index) {
-    if(!this->peopleStay.empty()){
-        this->people[index].push(this->peopleStay.front());
-        this->peopleStay.pop();
-    }
-    else{
-        std::cout<<"电梯里面没有人  别乱按好吗？？？"<<std::endl;
-    }
+  if (!this->peopleStay.empty()) {
+    this->people[index].push(this->peopleStay.front());
+    this->peopleStay.pop();
+  } else {
+    std::cout << "电梯里面没有人  别乱按好吗？？？" << std::endl;
+  }
 }
 
 void Elevator::press(int index, Person person) {
-    this->people[index].push(person);
+  this->people[index].push(person);
 }
 
 void Elevator::moveup() {
-    if (this->place <= 20) {
-        this->place++;
-    } else {
-        return;
-    }
+  if (this->place <= 20) {
+    this->place++;
+  } else {
+    return;
+  }
 }
 
 void Elevator::movedown() {
-    if (this->place >= 0) {
-        this->place--;
-    } else {
-        return;
-    }
+  if (this->place >= 0) {
+    this->place--;
+  } else {
+    return;
+  }
 }
 
 void Elevator::leave(int index) {
-    while (!this->people[index].empty()){
-        this->people[index].pop();
-        std::cout<<"clear"<<std::endl;
-    }
+  while (!this->people[index].empty()) {
+    this->people[index].pop();
+    std::cout << "clear" << std::endl;
+  }
 }
 
 void Elevator::display() {
-    for (int i = 0; i < fNum; i++) {
-        for (int j = 0; j < people[i].size(); j++) {
-            std::cout << "想要在" << i << "下楼" << people[i].front().getW() << std::endl;
-            people[i].push(people[i].front());
-            people[i].pop();
-        }
+  for (int i = 0; i < fNum; i++) {
+    for (int j = 0; j < people[i].size(); j++) {
+      std::cout << "想要在" << i << "下楼" << people[i].front().getW()
+                << std::endl;
+      people[i].push(people[i].front());
+      people[i].pop();
     }
+  }
 }
 
-int Elevator::getPeopleNum(){
-    int sum = 0;
-    for(int i=0;i<eNum;i++){
-        sum += this->people[i].size();
-    }
-    return sum;
+int Elevator::getPeopleNum() {
+  int sum = 0;
+  for (int i = 0; i < eNum; i++) {
+    sum += this->people[i].size();
+  }
+  return sum;
 }
 
 int Elevator::getW() {
-    int sum = 0;
-    for (int i = 0; i < fNum; i++) {
-        for (int j = 0; j < people[i].size(); j++) {
-            sum += people[i].front().getW();
-            people[i].push(people[i].front());
-            people[i].pop();
-        }
+  int sum = 0;
+  for (int i = 0; i < fNum; i++) {
+    for (int j = 0; j < people[i].size(); j++) {
+      sum += people[i].front().getW();
+      people[i].push(people[i].front());
+      people[i].pop();
     }
-    for (int j = 0; j < this->peopleStay.size(); j++) {
-        sum += this->peopleStay.front().getW();
-        this->peopleStay.push(this->peopleStay.front());
-        this->peopleStay.pop();
-    }
-    return sum;
+  }
+  for (int j = 0; j < this->peopleStay.size(); j++) {
+    sum += this->peopleStay.front().getW();
+    this->peopleStay.push(this->peopleStay.front());
+    this->peopleStay.pop();
+  }
+  return sum;
 }
 
-bool Elevator::getdirection() {
-    return this->direction;
-}
+bool Elevator::getdirection() { return this->direction; }
 
-void Elevator::setdirection(bool direction) {
-    this->direction = direction;
-}
+void Elevator::setdirection(bool direction) { this->direction = direction; }
 
-void Elevator::changedirection() {
-    this->direction = !this->direction;
-}
+void Elevator::changedirection() { this->direction = !this->direction; }
 
 void Elevator::move() {
-    if (this->getdirection()) {
-        this->place++;
-    } else {
-        this->place--;
-    }
+  if (this->getdirection()) {
+    this->place++;
+  } else {
+    this->place--;
+  }
 }
 
+int Elevator::getplace() { return this->place; }
 
-int Elevator::getplace() {
-    return this->place;
-}
+void Elevator::setplace(int place) { this->place = place; }
 
+int Elevator::getmaxW() { return this->maxW; }
 
-void Elevator::setplace(int place){
-    this->place = place;
-}
+void Elevator::pushpeopleStay(Person person) { this->peopleStay.push(person); }
 
-int Elevator::getmaxW() {
-    return this->maxW;
-}
+int Elevator::getStayNum() { return this->peopleStay.size(); }
 
-
-void Elevator::pushpeopleStay(Person person){
-    this->peopleStay.push(person);
-}
-
-
-int Elevator::getStayNum(){
-    return this->peopleStay.size();
-}
-
-Person Elevator::getpeopleStayfront(){
-    Person pp = this->peopleStay.front();
-    this->peopleStay.pop();
-    return pp;
+Person Elevator::getpeopleStayfront() {
+  Person pp = this->peopleStay.front();
+  this->peopleStay.pop();
+  return pp;
 }
 std::queue<Person> &Elevator::getpeople(int index) {
-    return this->people[index];
+  return this->people[index];
 }
-
